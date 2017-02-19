03a6eba97d37b3ceb27a8114c3f4c2dfb0a278a4
spankie <odohi.david@gmail.com>
2017-02-19 01:30:37 +0100

    persistent login

--- auth/login.js
+++ auth/login.js
@@ -1,14 +1,14 @@
-const {BrowserWindow} = require("electron")
+// const {BrowserWindow, ipcMain} = require("electron")
 const {config} = require("../config/config.js")
 const path = require('path')
-const url = require('url')
+// const fs = require('fs');
 
 var mysql = require("mysql");
 var connection = mysql.createConnection(config.dbAttendance)
 connection.connect();
 
-exports.login = function (username, password) {
-    
+exports.login = function (username, password, callback) {
+
     if(username == "" || password == "") return;
 
     console.log("username and password not empty");
@@ -19,34 +19,21 @@ exports.login = function (username, password) {
 
     connection.query(loginQuery, function(error, results, fields) {
         if(error) {
+            // event.returnValue = "Username or Password is incorrect";
+            callback("Username or Password is incorrect", username, password)
             console.log(error);
             return;
         }
         if (results && (results.length > 0)) {
-            console.log("results", results);
+            console.log("User Result:", results);
             // createWindow();
-            return "logged in";
+            callback("ok", username, password);
+            return;
         } else {
+            // event.returnValue = "Username or Password is incorrect";
+            callback("Username or Password is incorrect", username, password)
             console.log("no user");
         }
-        console.log("no user...");
+        // console.log("no user...");
     });
-    
-}
-
-function createWindow(){
-    let win = new BrowserWindow({width: 400, height: 200})
-    win.loadURL(url.format({
-                pathname: path.join(__dirname + "/pages/", 'index.html'),
-                protocol: 'file:',
-                slashes: true
-            }))
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    })
 }
--- pages/js/index.js
+++ pages/js/index.js
@@ -1,4 +1,4 @@
-const remote = require("electron").remote
+const {remote, ipcRenderer} = require("electron")
 const {auth, recreateWindow} = remote.require("./index.js")
 
 var loginButton = document.getElementById("login")
@@ -7,7 +7,16 @@ loginButton.addEventListener("click", () => {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
 
-    if(auth.login(username, password) == "logged in" ) {
-        recreateWindow();
+    var err = ipcRenderer.sendSync('login', {username: username, password: password})
+    console.log("error: ", err);
+    if (err) {
+        document.getElementById("error").value = err;
     }
+
+    // var status = auth.login(username, password);
+    // console.log("status from renderer : ", status);
+
+    // if(status == "logged in" ) {
+    //     recreateWindow();
+    // }
 })
